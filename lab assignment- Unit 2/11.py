34d8d128cf832581f00310106e93f5cb46ab0673
Upadhyay Hitarth <hitarth.upadhyay153038@marwadiuniversity.ac.in>
2026-08-12 10:22:00 +0530

    Add files via upload
    Assignment of Unit-2

---
+++ lab assignment- Unit 2/11.py
@@ -0,0 +1,24 @@
+#if statement
+age = 20
+if age >= 18:
+    print("You are eligible for voting!!!!")
+
+#if-else statement
+number = 5
+if number %2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+#if-elif-else
+marks = 75
+if marks >=90:
+    print("Grade A+")
+elif marks >=75:
+    print("Grade A")
+elif marks >=50:
+    print("Grade B")
+else:
+    print("Grade C")
+    
+               
