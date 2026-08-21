24e7675657dad26ee555b4edcfaf8c71df8b0261
Upadhyay Hitarth <hitarth.upadhyay153038@marwadiuniversity.ac.in>
2026-08-21 10:32:38 +0530

    Add files via upload
    Assignment of unit 2

---
+++ lab assignment- Unit 2/16.py
@@ -0,0 +1,26 @@
+# List
+fruits = ["apple", "mango", "orange", "banana"]
+
+print("List Elements:")
+for fruit in fruits:
+    print(fruit)
+
+
+# String
+name = "Python"
+
+print("\nString Characters:")
+for character in name:
+    print(character)
+
+
+# Dictionary
+student = {
+    "Name": "Hitarth",
+    "Age": 21,
+    "Course": "MCA"
+}
+
+print("\nDictionary Key and Values:")
+for key, value in student.items():
+    print(key, ":", value)
